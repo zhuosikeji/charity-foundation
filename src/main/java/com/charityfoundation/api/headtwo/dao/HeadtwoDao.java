45f188e94f2d22ef014e09ad4603e240ab7d9d4a
WangAo <1279990918@qq.com>
2019-03-29 21:17:56 +0800

    更新了几个接口

--- src/main/java/com/charityfoundation/api/headtwo/dao/HeadtwoDao.java
+++ src/main/java/com/charityfoundation/api/headtwo/dao/HeadtwoDao.java
@@ -1,10 +1,20 @@
 package com.charityfoundation.api.headtwo.dao;
 
-public class HeadtwoDao {
+import com.charityfoundation.api.headone.vo.FirstClassList;
+import com.charityfoundation.api.headtwo.vo.SecondClassList;
+import org.jeecgframework.minidao.annotation.Param;
+import org.jeecgframework.minidao.annotation.Sql;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HeadtwoDao {
 
     /**
      * 根据一级id获取二级id信息
      */
-
+    @Sql("select id,second_class_name,class_number FROM cf_article_second_classify WHERE first_classify_id=:firstClassId")
+    List<SecondClassList> selectSecondClassList(@Param("firstClassId")String firstClassId);
 
 }
